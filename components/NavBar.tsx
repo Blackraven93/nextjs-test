9bbb2888e7dd5315f10724248a95a62dd5232d0c
raven <reblackraven@gmail.com>
2022-03-02 00:07:31 +0900

    Feat: create navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const NavBar:NextPage = () => {
+  const router = useRouter();
+  return (
+    <nav>
+      <Link href="/">
+        <a className="home" style={{color: router.pathname === "/" ? "teal" : "tomato"}}>Home</a>
+      </Link>
+      <Link href="/about">
+        <a className="about" style={{color: router.pathname === "/about" ? "teal" : "tomato"}}>About</a>
+      </Link>
+    </nav>
+  )
+}
+
+export default NavBar;
---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+import NavBar from "../components/NavBar";
+
+const aBout:NextPage = () => {
+  return <div>
+    <NavBar />
+    <h1>About</h1>
+  </div>
+}
+
+export default aBout
