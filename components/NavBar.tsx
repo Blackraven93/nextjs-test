50933a77e81ebbb7dbc277ce46287d452601677f
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-06-10 18:21:59 +0900

    Chore: build pnpm

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,19 +1,29 @@
-import { NextPage } from "next";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import { NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
-const NavBar:NextPage = () => {
+const NavBar: NextPage = () => {
   const router = useRouter();
   return (
     <nav>
       <Link href="/">
-        <a className="home" style={{color: router.pathname === "/" ? "teal" : "tomato"}}>Home</a>
+        <a
+          className="home"
+          style={{ color: router.pathname === '/' ? 'teal' : 'tomato' }}
+        >
+          Home
+        </a>
       </Link>
       <Link href="/about">
-        <a className="about" style={{color: router.pathname === "/about" ? "teal" : "tomato"}}>About</a>
+        <a
+          className="about"
+          style={{ color: router.pathname === '/about' ? 'teal' : 'tomato' }}
+        >
+          About
+        </a>
       </Link>
     </nav>
-  )
-}
+  );
+};
 
 export default NavBar;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,13 @@
-import { NextPage } from "next";
-import NavBar from "../components/NavBar";
+import { NextPage } from 'next';
+import NavBar from '../components/NavBar';
 
-const aBout:NextPage = () => {
-  return <div>
-    <NavBar />
-    <h1>About</h1>
-  </div>
-}
+const aBout: NextPage = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>About</h1>
+    </div>
+  );
+};
 
-export default aBout
+export default aBout;
